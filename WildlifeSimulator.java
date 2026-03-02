52dd2d2e567ddfc16acd0a3ff581c64dbd1063c6
AHS CS Student <ahs-cs@ausd>
2026-03-02 14:35:33 -0800

    sdrftyui

--- WildlifeSimulator.java
+++ WildlifeSimulator.java
@@ -8,6 +8,8 @@ public class WildlifeSimulator {
     
     public WildlifeSimulator(int maxSpecies) {
         //TODO
+        species = new Species[maxSpecies];
+
     }
     
     /**
@@ -15,28 +17,45 @@ public class WildlifeSimulator {
      */
     public void addSpecies(Species s) {
         //TODO
+        species[speciesCount++] = s;
     }
     
     /**
      * Simulate one year of population changes for all species
      */
     public void simulateYear() {
-        //TODO
+        //call each species simulate year, loop species array
+        for(Species s : species){
+            if(s != null){
+                s.simulateYear();
+            }
+        }
+       
+
     }
     
     /**
      * Simulate multiple years
      */
     public void simulate(int years) {
-        //TODO
+        //loop for years, each year call simulate year
+        for(int i = 0; i<years; i++){
+            simulateYear();
+        }
+    }
+
+    public void writeYear(int year){
+        
     }
     
     /**
      * Get species at given index
      */
     public Species getSpecies(int index) {
-        //TODO
-        return null;
+        if(index >= this.species.length){
+            throw new IllegalArgumentException();
+        }
+        return this.species[index];
     }
     
     /**
@@ -45,13 +64,27 @@ public class WildlifeSimulator {
     public String getSpeciesInfo(int index) {
         return species[index].toString();
     }
+
+    public String toString(){
+        String m = "";
+        for(Species s: species){
+            if(s != null)
+            m += s.toString() + "\n";
+        
+    }
+        return m;
+    }
     
     /**
      * Get total wildlife count across all species
      */
     public double getTotalPopulation() {
-        //TODO
-        return 0.0;
+        long totalPop = 0;
+        for(Species s : species){
+            if(s != null)
+                totalPop += s.getPopulation();
+        }
+        return totalPop;
     }
     
     /**
@@ -59,7 +92,13 @@ public class WildlifeSimulator {
      */
     public int getMostPopulousIndex() {
         //TODO
-        return -1;
+        int maxPopIndex = 0;
+        for(int i = 0; i<speciesCount; i++){
+            if(this.species[maxPopIndex].getPopulation() < this.species[i].getPopulation()){
+                maxPopIndex = i;
+            }
+        }
+        return maxPopIndex;
     }
     
     /**
@@ -67,7 +106,13 @@ public class WildlifeSimulator {
      */
     public int getMostEndangeredIndex() {
         //TODO
-        return -1;
+        int minPopIndex = 0;
+        for(int i = 0; i<speciesCount; i++){
+            if(this.species[minPopIndex].getPopulation() > this.species[i].getPopulation()){
+                minPopIndex = i;
+            }
+        }
+        return minPopIndex;
     }
     
     public int getSpeciesCount() {
